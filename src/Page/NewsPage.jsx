e425a6b01bf75b54bbe7685bff50936e4c48fed9
BrentSoler <brntsoler@gmail.com>
2022-03-01 19:35:50 +0800

    fix:NewsPage Loading

--- src/Page/NewsPage.jsx
+++ src/Page/NewsPage.jsx
@@ -4,7 +4,7 @@ import { NewsCard, Spinner } from "../components/index";
 import { newsApi } from "../constant/index";
 
 function NewsPage() {
-	const [news, setNews] = useState([]);
+	const [news, setNews] = useState();
 
 	const loadNews = async () => {
 		try {
